1c7bdf92f8c020252e613f6ea99683f87dbdbd15
vendrixx <jeremy.germain24@gmail.com>
2017-04-17 14:41:17 +0200

    Modification totale de la structure du projet. Maintenant, grâce à la classe Test_NLP, les méthodes de StanfordNLP sont directement utilisées dans le code. Test avec un exemple simple.

--- src/Element_XML/Sentence.java
+++ src/Element_XML/Sentence.java
@@ -18,11 +18,17 @@ public class Sentence {
         this.id = id;
     }
 
-    public String toString() {
-        String str = "  Un noeud sentence qui contient : ";
-        str += "\n      un id qui vaut " + this.id;
+    /*public String toString() {
+        String str = "  Un noeud sentence d'id " + this.id + " qui contient : ";
         for(Tokens t : listTokens)
-            str += "\n\n        " + t;
+            str += "\n        " + t;
+        return str;
+    }*/
+
+    public String toString() {
+        String str = "";
+        for(Tokens t : listTokens)
+            str += t;
         return str;
     }
 }
--- src/TAL/MyXMLHandler.java
+++ src/TAL/MyXMLHandler.java
@@ -3,12 +3,18 @@ package TAL;
 import Element_XML.*;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
 import org.xml.sax.helpers.DefaultHandler;
+
+import java.util.ArrayList;
+import java.util.HashSet;
 
 /**
  * Created by jerem on 11/04/2017.
  */
 public class MyXMLHandler extends DefaultHandler {
+
+    //private final String lastElementToRead;
 
     private String node = null;
     private Root root;
@@ -18,21 +24,51 @@ public class MyXMLHandler extends DefaultHandler {
     private Tokens tokens;
     private Token token;
 
+    private ArrayList killer = new ArrayList();
+
+    private final StringBuilder stringBuilder = new StringBuilder(64);
+    private boolean elementOn = false;
+    private String elementValue = null;
+
+    private String tagCourant = "";
+
+    private String word = "";
+
+    /*public MyXMLHandler(String lastElementToRead) {
+        this.lastElementToRead = lastElementToRead;
+    }*/
+
+    public void notationDecl(String name, String publicId, String systemId) {
+        System.out.println(name + " - " + publicId + " - " + systemId);
+    }
+
+    public void error(SAXParseException e) throws SAXException {
+        System.out.println("ERROR : " + e.getMessage());
+        throw e;
+    }
+
+    public void fatalError(SAXParseException e) throws SAXException {
+        System.out.println("FATAL ERROR : " + e.getMessage());
+        throw e;
+    }
+
+    public void warning(SAXParseException e) throws SAXException {}
+
     /**
      * Début du parsing du document XML
      * @throws SAXException
      */
-    /*public void startDocument() throws SAXException {
+    public void startDocument() throws SAXException {
         System.out.println("Début du parsing");
-    }*/
+    }
 
     /**
      * Fin du parsing du document XML
      * @throws SAXException
      */
-    /*public void endDocument() throws SAXException {
+    public void endDocument() throws SAXException {
         System.out.println("Fin du parsing");
-    }*/
+    }
 
     /**
      * Intercepte les événements (méthode redéfinie)
@@ -41,8 +77,7 @@ public class MyXMLHandler extends DefaultHandler {
      * @throws SAXException
      */
     public void startElement(String namespaceURI, String lname, String qname, Attributes attrs) throws SAXException {
-        /*System.out.println("------------------------------------------");
-        System.out.println("qname = " + qname);*/
+        tagCourant = qname;
         node = qname;
 
         if(qname.equals("root")) {
@@ -59,33 +94,21 @@ public class MyXMLHandler extends DefaultHandler {
         } else if(qname.equals("token")) {
             token = new Token();
             token.setId(attrs.getValue(0));
+            //System.out.println(token);
         }
-        /*switch (qname) {
-            case "root":
-                root = new Root();
-                break;
-            case "document":
-                document = new Document();
-                break;
-            case "sentences":
-                sentences = new Sentences();
-                break;
-            case "sentence":
-                sentence = new Sentence();
-                sentence.setId(attrs.getValue(0));
-                break;
-            case "tokens":
-                tokens = new Tokens();
-                break;
-            case "token":
-                token = new Token();
-                token.setId(attrs.getValue(0));
-                break;
-        }*/
     }
 
     public void endElement(String uri, String localName, String qName) throws SAXException {
         //System.out.println("Fin de l'élément " + qName);
+
+        /*if(lastElementToRead.equals(qName)) {
+            throw new MySaxTerminatorException();
+        }*/
+        tagCourant = "";
+        /*stringBuilder.setLength(0);
+        if(qName.equals("token")) elementOn = true;
+        */
+
         if(qName.equals("document")) {
             root.addDocument(document);
         } else if(qName.equals("sentences")) {
@@ -97,23 +120,7 @@ public class MyXMLHandler extends DefaultHandler {
         } else if(qName.equals("token")) {
             tokens.addToken(token);
         }
-        /*switch (qName) {
-            case "document":
-                root.addDocument(document);
-                break;
-            case "sentences":
-                document.addSentences(sentences);
-                break;
-            case "sentence":
-                sentences.addSentence(sentence);
-                break;
-            case "tokens":
-                sentence.addTokens(tokens);
-                break;
-            case "token":
-                tokens.addToken(token);
-                break;
-        }*/
+
     }
 
 
@@ -124,53 +131,53 @@ public class MyXMLHandler extends DefaultHandler {
      * @param end longueur de la chaîne
      */
     public void characters(char[] data, int start, int end) {
-        //System.out.println("**********************************************");
-        String str = new String(data, start, end);
-        //System.out.println("Donnée du noeud " + node + " : " + str);
-        if(node.equals("word")) {
-            token.setWord(str);
-        } else if(node.equals("lemma")) {
-            token.setLemma(str);
-        } else if(node.equals("CharacterOffsetBegin")) {
-            token.setCharacterOffsetBegin(str);
-        } else if(node.equals("CharacterOffsetEnd")) {
-            token.setCharacterOffsetEnd(str);
-        } else if(node.equals("POS")) {
-            token.setPOS(str);
-        } else if(node.equals("NER")) {
-            token.setNER(str);
-        } else if(node.equals("NormalizedNER")) {
-            token.setNormalisedNER(str);
-        } else if(node.equals("Timex")) {
-            token.setTimex(str);
+        String donnees = new String(data, start, end);
+        if( !tagCourant.equals("")) {
+            if (tagCourant.equals("word")) {
+                //System.out.println("   Element = " + tagCourant + " , valeur = " + donnees);
+                word = donnees;
+            }
+
+            //String str = new String(data, start, end);
+            //node = stringBuilder.toString().trim();
+            node = tagCourant;
+
+            //System.out.println("Donnée du noeud " + node + " : " + str);
+            if (node.equals("word")) {
+                //str = new String(data, start, end);
+                //System.out.println(str + "------");
+                token.setWord(donnees);
+                //System.out.println(token);
+            } else if (node.equals("lemma")) {
+                token.setLemma(donnees);
+            } else if (node.equals("CharacterOffsetBegin")) {
+                token.setCharacterOffsetBegin(donnees);
+            } else if (node.equals("CharacterOffsetEnd")) {
+                token.setCharacterOffsetEnd(donnees);
+            } else if (node.equals("POS")) {
+                token.setPOS(donnees);
+            } else if (node.equals("NER")) {
+                token.setNER(donnees);
+                if (donnees.equals("PERSON") && word.charAt(0) == 'H' && !killer.contains(sentence)) {
+                    System.out.println("Add killer");
+                    killer.add(sentence);
+                }
+            } else if (node.equals("NormalizedNER")) {
+                token.setNormalisedNER(donnees);
+            } else if (node.equals("Timex")) {
+                token.setTimex(donnees);
+            }
         }
-        /*switch (node) {
-            case "word":
-                token.setWord(str);
-                break;
-            case "lemma":
-                token.setLemma(str);
-                break;
-            case "CharacterOffsetBegin":
-                token.setCharacterOffsetBegin(str);
-                break;
-            case "CharacterOffsetEnd":
-                token.setCharacterOffsetEnd(str);
-                break;
-            case "POS":
-                token.setPOS(str);
-                break;
-            case "NER":
-                token.setNER(str);
-                break;
-            case "Normalized NER":
-                token.setNormalisedNER(str);
-                break;
-            case "Timex":
-                token.setTimex(str);
-                break;
-        }*/
+
+    }
+
+
+    public void ignorableWhitespace(char[] data, int start, int end) throws SAXException {
+        System.out.println("espaces inutiles rencontres : ..." + new String(data, start, end) +  "...");
     }
 
     public Root getRoot() { return root; }
+    public ArrayList getKiller() { return killer; }
+
+    public class MySaxTerminatorException extends SAXException {}
 }
